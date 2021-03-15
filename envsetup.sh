5c6a3c8b221ed9b66702c53bcf549633afa4264c
ittat <ittat@live.com>
2021-03-15 08:33:14 +0800

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -136,6 +136,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^b2g_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^b2g_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        CUSTOM_BUILD=
+    fi
+    export CUSTOM_BUILD
+    
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -638,6 +646,8 @@ function lunch()
         return 1
     fi
 
+    check_product $product
+    
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     TARGET_PLATFORM_VERSION=$version \
